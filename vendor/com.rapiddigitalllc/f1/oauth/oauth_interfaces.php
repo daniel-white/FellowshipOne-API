897d3ebf1a137b99bd5f17585f8d28bc56e3f6af
Daniel A. White <dwhite@thetrueaplus.com>
2014-07-22 19:35:37 -0400

    adding minimum abstraction surface needed for oauth

---
+++ vendor/com.rapiddigitalllc/f1/oauth/oauth_interfaces.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/**
+ * F1 API
+ * @author Daniel Boorn - daniel.boorn@gmail.com
+ * @copyright Daniel Boorn
+ * @license Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
+ * @namespace F1
+ */
+
+namespace f1\oauth;
+
+
+
